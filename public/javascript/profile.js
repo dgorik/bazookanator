43d8366063d3706f40230e467771da3740532b0b
Daniel Gorbachev <dg186533@gmail.com>
2024-06-30 11:29:27 -0400

    Added links to social media icons

---
+++ public/javascript/profile.js
@@ -0,0 +1,9 @@
+function togglePopup() {
+    const overlay = document.getElementById('popupOverlay');
+    overlay.classList.toggle('show');
+}
+
+function menuPopup() {
+    const overlay = document.getElementById('leftColumn');
+    overlay.classList.toggle('show');
+}
