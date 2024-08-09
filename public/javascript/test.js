c7f4156bd23454ec45d3004b91f19a429a880e0d
Daniel Gorbachev <dg186533@gmail.com>
2024-08-09 17:43:13 -0400

    Worked on more questions

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -2,8 +2,9 @@
 
 function checkAnswer1() {
     var answer = document.getElementById('answer1').value.toLowerCase();
-    if (answer == 'london') { // Correct answer for the first question
-        alert("Well, bloody done! You've sorted the first one - two more to go!")
+    let correct_answer = 'london'
+    if (answer == correct_answer) { // Correct answer for the first question
+        alert("Well, bloody done! You've sorted the first one - 4 more to go!")
         document.getElementById('question1').classList.remove('visible');
         document.getElementById('question2').classList.add('visible');
 
@@ -14,7 +15,6 @@ function checkAnswer1() {
 
 function checkAnswer2() {
     var answer = document.getElementById('question2_choices').value;
-    console.log(answer)
     let correct_answer = 'answer3'
     if (answer == correct_answer) { // Correct answer for the second question
         alert('Correct! Questionnaire completed.');
@@ -26,9 +26,25 @@ function checkAnswer2() {
 }
 
 function checkAnswer3() {
-    var answer = document.getElementById('question2_choices').value;
-    console.log(answer)
-    let correct_answer = 'answer3'
+    var answer = document.getElementById('answer3').value.toLowerCase();
+    let correct_answer = 'kroger'
+    if (answer == correct_answer) { // Correct answer for the second question
+        alert('Correct! Questionnaire completed.');
+        document.getElementById('question3').classList.remove('visible');
+        document.getElementById('question4').classList.add('visible');
+        setTimeout(function () {
+            document.getElementById('alan_taco').style.display = 'block';
+        }, 5000);
+
+
+    } else {
+        alert('Wrong answer. Try again.');
+    }
+}
+
+function checkAnswer4() {
+    var answer = document.getElementById('answer4').value.toLowerCase();
+    let correct_answer = 'alan'
     if (answer == correct_answer) { // Correct answer for the second question
         alert('Correct! Questionnaire completed.');
     } else {
