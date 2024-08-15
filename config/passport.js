9e82784113896414f819c26889bec42ced5a7fdc
Daniel Gorbachev <dg186533@gmail.com>
2024-08-15 16:45:06 -0400

    changed a header

--- config/passport.js
+++ config/passport.js
@@ -5,29 +5,22 @@ const User = require("../models/User");
 module.exports = function (passport) {
   passport.use(
     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
-        if (err) {
+      User.findOne({ email: email.toLowerCase() })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, { msg: `Email ${email} not found.` });
+          }
+          if (!user.password) {
+            return done(null, false, {
+              msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+            });
+          }
+          // Compare passwords here or other logic
+          return done(null, user);
+        })
+        .catch((err) => {
           return done(err);
-        }
-        if (!user) {
-          return done(null, false, { msg: `Email ${email} not found.` });
-        }
-        if (!user.password) {
-          return done(null, false, {
-            msg:
-              "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
-          });
-        }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err);
-          }
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { msg: "Invalid email or password." });
         });
-      });
     })
   );
 
