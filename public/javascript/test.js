da091c08aa58729c258949a3a73155203b82722a
Daniel Gorbachev <dg186533@gmail.com>
2024-10-23 18:26:50 -0400

    Worked on quizz questions

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -46,46 +46,74 @@ function checkAnswer2() {
 function checkAnswer3() {
     var answer = document.getElementById('answer3').value.toLowerCase();
     var requestType = "question3"
-    if (answer == correct_answer) { // Correct answer for the second question
-        alert("Splendid! Only two to go, keep your pecker up!");
-        document.getElementById('question3').classList.remove('visible');
-        document.getElementById('question4').classList.add('visible');
-        setTimeout(function () {
-            document.getElementById('alan_taco').style.display = 'block';
-        }, 4000);
-
-
-    } else {
-        alert('Wrong answer. Try again.');
-    }
+    fetch('/quizCheck', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputData: answer, requestType: requestType }),
+    }).then(response => response.json())
+        .then(data => {
+            if (data) {
+                alert("Splendid! Only two to go, keep your pecker up!");
+                document.getElementById('question3').classList.remove('visible');
+                document.getElementById('question4').classList.add('visible');
+                setTimeout(function () {
+                    document.getElementById('alan_taco').style.display = 'block';
+                }, 4000);
+            }
+            else {
+                alert('Wrong answer. Try again.');
+            }
+        })
 }
 
 function checkAnswer4() {
     var answer = document.getElementById('answer4').value.toLowerCase();
     var requestType = "question4"
-    if (answer == correct_answer) { // Correct answer for the second question
-        alert("Blimey! Just one to go and it's tea time!");
-        document.getElementById('question4').classList.remove('visible');
-        document.getElementById('question5').classList.add('visible');
-        setTimeout(function () {
-            document.getElementById('harry_lick').style.display = 'block';
-        }, 4000);
-    } else {
-        alert('Wrong answer. Try again.');
-    }
+    fetch('/quizCheck', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputData: answer, requestType: requestType }),
+    }).then(response => response.json())
+        .then(data => {
+            if (data) {
+                alert("Blimey! Just one to go and it's tea time!");
+                document.getElementById('question4').classList.remove('visible');
+                document.getElementById('question5').classList.add('visible');
+                setTimeout(function () {
+                    document.getElementById('harry_lick').style.display = 'block';
+                }, 4000);
+            }
+            else {
+                alert('Wrong answer. Try again.');
+            }
+        })
 }
 
 function checkAnswer5() {
     var answer = document.getElementById('answer5').value.toLowerCase();
     var requestType = "question5"
-    if (answer == correct_answer) { // Correct answer for the second question
-        alert("Jolly good! All questions done and dusted! Get ready to be redirected")
-        setTimeout(function () {
-            window.location.href = '/signup';
-        }, 3000);
-    } else {
-        alert('Wrong answer. Try again.');
-    }
+    fetch('/quizCheck', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputData: answer, requestType: requestType }),
+    }).then(response => response.json())
+        .then(data => {
+            if (data) {
+                alert("Jolly good! All questions done and dusted! Get ready to be redirected")
+                setTimeout(function () {
+                    window.location.href = '/signup';
+                }, 3000);
+            }
+            else {
+                alert('Wrong answer. Try again.');
+            }
+        })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
